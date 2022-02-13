0241e109b8752e13640cb8f53dd88a46a44d4905
Sorin373 <sorin.andrei.tudose@gmail.com>
2022-02-13 11:49:10 +0200

    Website update
    Improved project navigation bar styles.
    Added live background to project page.
    Added collapsible to project page.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const swup = new Swup()
+/*Web windows animation*/
+window.addEventListener("beforeunload", function () {
+    document.body.classList.add("animate-out");
+  });
+
+/*Menu dropdown*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    for(item of coll){
+      item.classList.remove("active");
+      item.nextElementSibling.style.display = "none";
+    }
+      
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
