1cb558fdf9cb67a4aea4b9f2d39ebb8db0bb96d7
Sorin373 <sorin.andrei.tudose@gmail.com>
2022-02-17 12:46:42 +0200

    Collapsible-update
    Added stylesheet - text styles

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ window.addEventListener("beforeunload", function () {
     document.body.classList.add("animate-out");
   });
 
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
+/*Navigation bar animation (Projects page)*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
   
-  for (i = 0; i < coll.length; i++) {
+for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
@@ -17,5 +18,29 @@ window.addEventListener("beforeunload", function () {
       }
     });
   }
+
+/*Words animation*/
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline()
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 1300,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 1300,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  })
+
 
 
